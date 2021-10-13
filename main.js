2cacf64580fd5ea9455b2b76505e40b05b150f49
Carlos Gomez <carlosandres0741@gmail.com>
2021-10-13 08:48:15 -0500

    Add: Random color on load

--- main.js
+++ main.js
@@ -2,24 +2,24 @@ const $color = document.querySelector('#color');
 const $button = document.querySelector('#button');
 const $main = document.querySelector('main');
 
-const chars = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','f',];
+const chars = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','f'];
 
 const chooseColor = () => {
-  let index = Math.round(Math.random() * (chars.length - 1));
+  let index;
+  let color = '#';
   console.log(index);
 
-  return chars[index];
-}
-
-$button.addEventListener('click', () => {
-  let color = '#';
-
   for (let i = 0; i < 6; i++) {
-    let char = chooseColor();
+    index = Math.round(Math.random() * (chars.length - 1));
+    let char = chars[index];
     color += char;
   }
 
   $main.style = `background: ${color}`
   $color.innerHTML = color;
-})
+}
+
+window.addEventListener('load', chooseColor)
+
+$button.addEventListener('click', chooseColor)
 
